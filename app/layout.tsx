3027a301572268d4e5be8cfa5621353e25c55847
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2024-11-09 16:50:42 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-const mulish = Mulish({ subsets: ["latin-ext"] });
+const mulish = Mulish({
+  subsets: ["latin-ext"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${mulish.className} dark antialiased`}>
-        <ClerkProvider appearance={{ baseTheme: dark }}>
-          {children}
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+          }}
+        >
+          <div className="flex h-full flex-col overflow-hidden">{children}</div>
         </ClerkProvider>
       </body>
     </html>
