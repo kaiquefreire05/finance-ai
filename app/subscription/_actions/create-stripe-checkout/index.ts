82af1c68df3ae0367f9d5146bbc42ecdcfbaffbc
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2024-11-09 23:26:50 -0300

    feat: add stripe webhook that handles subscription

--- app/subscription/_actions/create-stripe-checkout/index.ts
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -8,23 +8,28 @@ export const createStripeCheckout = async () => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
   if (!process.env.STRIPE_SECRET_KEY) {
     throw new Error("Stripe secret key not found");
   }
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    subscription_data: { metadata: { clerk_user_id: userId } },
+    success_url: "http://localhost:3000",
+    cancel_url: "http://localhost:3000",
+    subscription_data: {
+      metadata: {
+        clerk_user_id: userId,
+      },
+    },
     line_items: [
-      { price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID, quantity: 1 },
+      {
+        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
+        quantity: 1,
+      },
     ],
-    success_url: "https://localhost:3000/",
-    cancel_url: "https://localhost:3000/",
   });
   return { sessionId: session.id };
 };
--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -3,9 +3,12 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
-  const handleAdquirePlanClick = async () => {
+  const { user } = useUser();
+  const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
       throw new Error("Stripe publishable key not found");
@@ -14,16 +17,26 @@ const AcquirePlanButton = () => {
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     );
     if (!stripe) {
-      throw new Error("Failed to load Stripe");
+      throw new Error("Stripe not found");
     }
-
     await stripe.redirectToCheckout({ sessionId });
   };
-
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium";
+  if (hasPremiumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       className="w-full rounded-full font-bold"
-      onClick={handleAdquirePlanClick}
+      onClick={handleAcquirePlanClick}
     >
       Adquirir plano
     </Button>
