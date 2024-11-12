13fba1ae26649320a4373a90ce6cd69e335f4840
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2024-11-12 14:08:35 -0300

    chore: resolving dependencies errors

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import Markdown from "react-markdown";
 import {
   Dialog,
   DialogClose,
@@ -13,71 +12,92 @@ import {
   DialogTrigger,
 } from "@/app/_components/ui/dialog";
 import { BotIcon, Loader2Icon } from "lucide-react";
-import { generateIaReport } from "../_actions/generate-ia-report";
+import { generateAiReport } from "../_actions/generate-ia-report";
 import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
-import remarkGfm from "remark-gfm";
+import Markdown from "react-markdown";
+import Link from "next/link";
 
 interface AiReportButtonProps {
+  hasPremiumPlan: boolean;
   month: string;
 }
 
-const AiReportButton = ({ month }: AiReportButtonProps) => {
-  const [reportIsLoading, setReportIsLoading] = useState(false);
+const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>(null);
+  const [reportIsLoading, setReportIsLoading] = useState(false);
   const handleGenerateReportClick = async () => {
     try {
       setReportIsLoading(true);
-      const report = await generateIaReport(month);
-      console.log({ report });
-      setReport(report);
+      const aiReport = await generateAiReport({ month });
+      console.log({ aiReport });
+      setReport(aiReport);
     } catch (error) {
       console.error(error);
     } finally {
       setReportIsLoading(false);
     }
   };
-  console.log({ report });
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="ghost" className="font-bold">
-            <BotIcon />
-            Relatório IA
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Relatório com IA</DialogTitle>
-            <DialogDescription>
-              Use inteligência artificial para gerar um relatório com insights
-              sobre suas finanças.
-            </DialogDescription>
-          </DialogHeader>
-          {report && (
-            <ScrollArea className="prose prose-slate max-h-[450px] text-white marker:text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
-              <Markdown remarkPlugins={[remarkGfm]}>{report}</Markdown>
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          setReport(null);
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          Relatório IA
+          <BotIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px]">
+        {hasPremiumPlan ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Use inteligência artificial para gerar um relatório com insights
+                sobre suas finanças.
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+              <Markdown>{report}</Markdown>
             </ScrollArea>
-          )}
-          <DialogFooter>
-            <DialogClose>
-              <Button variant="ghost" className="font-bold">
-                Cancelar
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button
+                onClick={handleGenerateReportClick}
+                disabled={reportIsLoading}
+              >
+                {reportIsLoading && <Loader2Icon className="animate-spin" />}
+                Gerar relatório
               </Button>
-            </DialogClose>
-            <Button
-              onClick={handleGenerateReportClick}
-              disabled={reportIsLoading}
-              className="font-bold"
-            >
-              {reportIsLoading && <Loader2Icon className="mr-1 animate-spin" />}
-              {reportIsLoading ? "Gerando relatório..." : "Gerar relatório"}
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Você precisa de um plano premium para gerar relatórios com IA.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button asChild>
+                <Link href="/subscription">Assinar plano premium</Link>
+              </Button>
+            </DialogFooter>
+          </>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 };
 
